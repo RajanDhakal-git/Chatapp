b89ca7d658cf9997377006e02038f606553523c4
RajanDhakal-git <90204005+RajanDhakal-git@users.noreply.github.com>
2024-11-30 13:38:05 +0545

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const {Server} = require('socket.io')
-
+app.get("/",(req,res)=> res.send("running"))
 let server = app.listen(3000)
 let io = new Server(server ,{
     cors:{
