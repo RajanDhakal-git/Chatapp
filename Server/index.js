1f33a6ca529f730ac9d4fa1ce70a971e0dbee5bf
RajanDhakal-git <90204005+RajanDhakal-git@users.noreply.github.com>
2024-11-30 12:58:02 +0545

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,7 @@ const {Server} = require('socket.io')
 let server = app.listen(3000)
 let io = new Server(server ,{
     cors:{
-        origin:'http://localhost:5173',
+        origin:'https://chatapp-virid-eta.vercel.app/',
         methods:['GET' , 'POST']
     }
 })
